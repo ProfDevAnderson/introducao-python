6c4110058a03daa61001011994457e31776795f9
Anderson Soares <anderson@lsdi.ufma.br>
2021-08-18 11:11:55 -0300

    Adicionando aresta bidirecional

--- Lista_Adjacencia.py
+++ Lista_Adjacencia.py
@@ -5,24 +5,26 @@ def inicializar_lista():
 
 def mostrar_grafo():
     for i in range(vertices):
-        print(i+1, ': ', grafo[i])
+        print(i+1, ' : ', grafo[i])
 
 def mostrar_grafo_detalhado():
     for i in range(vertices):
-        print(i+1, ' :', end = ' ')
-        for j in grafo[i]:
-            print(j, end = ' ')
         print('')
+        print(i+1, ' : ', end=' ')
+        for j in grafo[i]:
+            print(j, end= ' ')
 
 def adicionar_aresta(vertice_a, vertice_b):
     grafo[vertice_a-1].append(vertice_b)
+    if vertice_a != vertice_b:
+        grafo[vertice_b-1].append(vertice_a)
 
 grafo = []
 vertices = int (input('Qual a quantidade de vértices no grafo? '))
 inicializar_lista()
 adicionar_aresta(1, 2)
 adicionar_aresta(1, 3)
-adicionar_aresta(2, 1)
-adicionar_aresta(3, 1)
+# adicionar_aresta(2, 1)
+# adicionar_aresta(3, 1)
 mostrar_grafo()
 mostrar_grafo_detalhado()
